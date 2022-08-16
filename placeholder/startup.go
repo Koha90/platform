e508484fd5b2b6087f6cf14524e0592bec11db2a
koha90 <aleksey.jake@gmail.com>
2022-08-16 17:55:36 +0500

    create pipelines

---
+++ placeholder/startup.go
@@ -0,0 +1,29 @@
+package placeholder
+
+import (
+	"sync"
+
+	"platform/http"
+	"platform/pipeline"
+	"platform/pipeline/basic"
+	"platform/services"
+)
+
+func createPipeline() pipeline.RequestPipeline {
+	return pipeline.CreatePipeline(
+		&basic.ServicesComponent{},
+		&basic.LoggingComponent{},
+		&basic.ErrorComponent{},
+		&basic.StaticFileComponent{},
+		&SimpleMessageComponent{},
+	)
+}
+
+func Start() {
+	results, err := services.Call(http.Serve, createPipeline())
+	if err == nil {
+		(results[0].(*sync.WaitGroup)).Wait()
+	} else {
+		panic(err)
+	}
+}
