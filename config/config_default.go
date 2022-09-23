c37b48bb23bce1dd4a93c3473623a33650b7f6fa
koha90 <aleksey.jake@gmail.com>
2022-09-23 14:45:18 +0500

    updated func get in config_default.go. Added pointer for DefaultConfig

--- config/config_default.go
+++ config/config_default.go
@@ -6,7 +6,7 @@ type DefaultConfig struct {
 	configData map[string]interface{}
 }
 
-func (c DefaultConfig) get(name string) (result interface{}, found bool) {
+func (c *DefaultConfig) get(name string) (result interface{}, found bool) {
 	data := c.configData
 	for _, key := range strings.Split(name, ":") {
 		result, found = data[key]
