686c0104d3c998cf9726e88efdff759e4debfa2d
koha90 <koha90@github.com>
2023-03-30 14:57:49 +0500

    initial commit without readme.md

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+// package main is general package for platform
+package main
+
+import "fmt"
+
+// writeMessage write "Hi, Platform" in terminal
+func writeMessage() {
+	fmt.Println("Hi, Platform!")
+}
+
+func main() {
+	writeMessage()
+}
