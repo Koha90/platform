f00f8fab2761c8c029ee00cadc827de19f5bf72f
koha90 <koha90@github.com>
2023-04-04 15:27:21 +0500

    changed color log level

--- pkg/logging/default_create.go
+++ pkg/logging/default_create.go
@@ -11,11 +11,11 @@ import (
 )
 
 var (
-	trace = fmt.Sprintf("%c[1;46;34m%s%c[0m", 0x1B, "[TRACE]: ", 0x1B)
-	debug = fmt.Sprintf("%c[1;47;34m%s%c[0m", 0x1B, "[DEBUG]: ", 0x1B)
+	trace = fmt.Sprintf("%c[1;40;36m%s%c[0m", 0x1B, "[TRACE]: ", 0x1B)
+	debug = fmt.Sprintf("%c[1;40;37m%s%c[0m", 0x1B, "[DEBUG]: ", 0x1B)
 	info  = fmt.Sprintf("%c[1;40;34m%s%c[0m", 0x1B, "[INFO]: ", 0x1B)
-	warn  = fmt.Sprintf("%c[1;43;34m%s%c[0m", 0x1B, "[WARN]: ", 0x1B)
-	fatal = fmt.Sprintf("%c[1;45;34m%s%c[0m", 0x1B, "[FATAL]: ", 0x1B)
+	warn  = fmt.Sprintf("%c[1;40;33m%s%c[0m", 0x1B, "[WARN]: ", 0x1B)
+	fatal = fmt.Sprintf("%c[1;40;31m%s%c[0m", 0x1B, "[FATAL]: ", 0x1B)
 )
 
 // NewDefaultLogger create default logger
