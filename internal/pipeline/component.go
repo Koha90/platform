cff06c9ba8b588754b15a7488ae47a30ecbdecaf
koha90 <koha90@github.com>
2023-04-04 16:24:03 +0500

    create function for create function

---
+++ internal/pipeline/component.go
@@ -0,0 +1,27 @@
+// Package pipeline middleware application
+package pipeline
+
+import "net/http"
+
+// ComponentContext struct of middleware application
+type ComponentContext struct {
+	*http.Request
+	http.ResponseWriter
+	error
+}
+
+// Error ...
+func (mwc *ComponentContext) Error(err error) {
+	mwc.error = err
+}
+
+// GetErr ...
+func (mwc *ComponentContext) GetErr() error {
+	return mwc.error
+}
+
+// MiddlewareComponent interface of function
+type MiddlewareComponent interface {
+	Init()
+	ProcessRequest(context *ComponentContext, next func(*ComponentContext))
+}
---
+++ internal/pipeline/pipe.go
@@ -0,0 +1,40 @@
+// Package pipeline ...
+package pipeline
+
+import "net/http"
+
+// RequestPipeline ...
+type RequestPipeline func(*ComponentContext)
+
+var emptyPipeline RequestPipeline = func(cc *ComponentContext) {
+	/* do nothing */
+}
+
+// CreatePipeline ...
+func CreatePipeline(components ...MiddlewareComponent) RequestPipeline {
+	f := emptyPipeline
+	for i := len(components) - 1; i >= 0; i-- {
+		currentComponent := components[i]
+		nextFunc := f
+		f = func(context *ComponentContext) {
+			if context.error == nil {
+				currentComponent.ProcessRequest(context, nextFunc)
+			}
+		}
+		currentComponent.Init()
+	}
+
+	return f
+}
+
+// ProcessRequest ...
+func (pl RequestPipeline) ProcessRequest(req *http.Request, resp http.ResponseWriter) error {
+	ctx := ComponentContext{
+		Request:        req,
+		ResponseWriter: resp,
+	}
+
+	pl(&ctx)
+
+	return ctx.error
+}
